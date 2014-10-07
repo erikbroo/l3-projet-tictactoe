3a45099bef747bf5a1d755fd7ca0257fff244ae7
Barat Joris <joris.barat@etu.univ-orleans.fr>
2014-10-07 19:00:44 +0200

    Type enregistrement pour un joueur. Possibilité d'ajouter un nom, de modifier le symbole du joueur et de vérifier si le joueur est un IA ou non

--- morpion.adb
+++ morpion.adb
@@ -27,14 +27,20 @@ procedure Morpion is
    -- Variables
    Plateau : T_Tableau;
    FinDePartie : T_FinDePartie  := Non;
-   
-   J : T_Joueur;
-   
    NbreCasesRempli : Integer := 0;
    I : Integer := 1; -- Numéro du joueur courant
    MaxJoueurs : constant Integer := 2;
    
-   G : Aleatoire.Generator; 
+   G : Aleatoire.Generator;
+   
+   -- enregistrement de Joueur
+   type Joueur is record
+     NumJoueur : T_Joueur;
+     Nom : Unbounded_String;
+     Symbole : Character;
+     IA : Boolean;
+   end record;
+
    
    -- ** Affiche **
    -- => Affiche un plateau 
@@ -63,8 +69,6 @@ procedure Morpion is
 	
       end loop;
       New_Line;
-      Put_Line("Joueur 1 => O");
-      Put_Line("Joueur 2 => X");
    end Affiche;
    
    
@@ -93,87 +97,113 @@ procedure Morpion is
    -- => Demande au joueur la position de sa case choisie
    -- => Vérifie que la case n'est pas occupée
    -- => Si oui, l'ajoute dans le plateau 
-  procedure Joue (T : in out T_Tableau; J : T_Joueur) is 
-     OK : Boolean := False;
-     X,Y : Character := ' ';
-  begin
-     while (not OK and NbreCasesRempli <  (Ligne'width * Colonne'width) ) loop
-	Saisie(X,Y);
-	
-	if T(X,Y) = ' ' then 
-	   case J is 
-	      when Joueur1 => T(X,Y) := 'O';
-	      when Joueur2 => T(X,Y) := 'X';
-	   end case;
-	   OK := True;
-	   NbreCasesRempli := NbreCasesRempli + 1; 
-	else
-	   Put_Line("Cette case est déjà prise...");
-	end if;
-	
-     end loop;
-  end Joue;  
-  
-  -- ** Nul **
-  -- => Teste si le plateau est rempli (ce qui revient à dire qu'il y a une égalité)
-  function Nul return T_FinDePartie is 
-  begin
-     if NbreCasesRempli >= (Ligne'width * Colonne'width) then 
-	return Egalite;
-     else 
-	return Non;
-     end if;
-  end Nul;
-     
-  -- ** Gagne **
-  -- => Vérifie si un joueur a gagné ou non 
-  function Gagne (T : T_Tableau) return T_FinDePartie is  
-  begin
-     
-     -- Lignes
-     for X in Ligne loop
-	if (T(X,'A') = T(X,'B') and T(X,'B') = T(X,'C') and T(X,'A') /= ' ' ) then 
-	   FinDePartie := Victoire;
-	end if;
-     end loop;
-     
-     -- Colonnes
-     for Y in Colonne loop
-	if (T('1',Y) = T('2',Y) and T('2',Y) = T('3',Y) and T('1',Y) /= ' ')
-	then 
-	   FinDePartie := Victoire;
-	end if;
-     end loop;
-     
-     -- Diagonales
-     if (T('1','A') = T('2','B') and  T('2','B') = T('3','C') and T('1','A') /= ' ') then 
-       FinDePartie := Victoire;
-     end if;
-     
-     if (T('1','C') = T('2','B') and T('2','B') = T('3','A') and T('1','C') /= ' ') then
-       FinDePartie := Victoire;
-     end if;
-     return FinDePartie;
-  end Gagne;
-  
-  
-  -- **Initialisation**
-  -- => Initialise un plateau vide (toutes les cases du tableau ont le caractère ' ')
-  function Initialisation return T_Tableau is 
-     T : T_Tableau;
-  begin 
-       for Y in Colonne loop	 
-	  for X in Ligne loop
-	     T(X,Y) := ' ';
-	end loop;
-       end loop;
-       
-       return T;
-       
-  end Initialisation;
-    
-  Ch : Unbounded_String;
-  J1, J2 : Unbounded_String;
+   procedure Joue (T : in out T_Tableau; J : Joueur) is 
+      -- OK boolean vérifiant si la case n'est pas déjà occupée par un joueur
+      OK : Boolean := False;
+      X,Y : Character := ' ';
+   begin
+      while (not OK and NbreCasesRempli <  (Ligne'width * Colonne'width) ) loop
+	 if (not J.IA) then 
+	    Saisie(X,Y);
+	 -- else 
+	    -- Renvoie les coordonnées de la case de l'IA 
+	 end if;
+	 
+	 if T(X,Y) = ' ' then 
+	    case J.NumJoueur is 
+	       when Joueur1 => T(X,Y) := 'O';
+	       when Joueur2 => T(X,Y) := 'X';
+	    end case;
+	    OK := True;
+	    NbreCasesRempli := NbreCasesRempli + 1; 
+	 else
+	    Put_Line("Cette case est déjà prise...");
+	 end if;
+	 
+      end loop;
+   end Joue;  
+   
+   -- ** Nul **
+   -- => Teste si le plateau est rempli (ce qui revient à dire qu'il y a une égalité)
+   function Nul return T_FinDePartie is 
+   begin
+      if NbreCasesRempli >= (Ligne'width * Colonne'width) then 
+	 return Egalite;
+      else 
+	 return Non;
+      end if;
+   end Nul;
+   
+   -- ** Gagne **
+   -- => Vérifie si un joueur a gagné ou non 
+   function Gagne (T : T_Tableau) return T_FinDePartie is  
+   begin
+      
+      -- Lignes
+      for X in Ligne loop
+	 if (T(X,'A') = T(X,'B') and T(X,'B') = T(X,'C') and T(X,'A') /= ' ' ) then 
+	    FinDePartie := Victoire;
+	 end if;
+      end loop;
+      
+      -- Colonnes
+      for Y in Colonne loop
+	 if (T('1',Y) = T('2',Y) and T('2',Y) = T('3',Y) and T('1',Y) /= ' ')
+	 then 
+	    FinDePartie := Victoire;
+	 end if;
+      end loop;
+      
+      -- Diagonales
+      if (T('1','A') = T('2','B') and  T('2','B') = T('3','C') and T('1','A') /= ' ') then 
+	 FinDePartie := Victoire;
+      end if;
+      
+      if (T('1','C') = T('2','B') and T('2','B') = T('3','A') and T('1','C') /= ' ') then
+	 FinDePartie := Victoire;
+      end if;
+      return FinDePartie;
+   end Gagne;
+   
+   
+   -- **Initialisation**
+   -- => Initialise un plateau vide (toutes les cases du tableau ont le caractère ' ')
+   function Initialisation return T_Tableau is 
+      T : T_Tableau;
+   begin 
+      for Y in Colonne loop	 
+	 for X in Ligne loop
+	    T(X,Y) := ' ';
+	 end loop;
+      end loop;
+      
+      return T;
+      
+   end Initialisation;
+   
+   function Initialisation_Joueur (NJ : T_Joueur) return Joueur is
+      J : Joueur;
+   begin   
+      -- Saisie du nom du joueur 
+      Put_Line("Entrez le nom du " & T_Joueur'Image(NJ) & " :" );
+      J.Nom := Get_Line;
+      case NJ is
+	 -- Définit le symbole, le numéro du joueur 1 
+	 when Joueur1 => 
+	   J.Symbole := 'O';
+	   J.NumJoueur := Joueur1;
+	 -- Définit le symbole, le numéro du joueur 2
+	 when Joueur2 =>
+	   J.Symbole := 'X';
+	   J.NumJoueur := Joueur2;
+      end case;
+      J.IA := False;
+      return J;
+   end Initialisation_Joueur;
+      
+   
+   Ch : Unbounded_String;
+   J1, J2, JoueurCourant : Joueur;
 begin
    -- Initialise le générateur de nombres aléatoires 
    Reset(G);
@@ -181,7 +211,10 @@ begin
    Put_Line("Bienvenue dans le jeu du (Super) Morpion !...");
    Put_Line("Faites [Entrée] pour commencer !");
    Ch := Get_Line;
-    
+   
+   J1 := Initialisation_Joueur(Joueur1);
+   J2 := Initialisation_Joueur(Joueur2);
+   
    --Initialisation
    Plateau := Initialisation;
    I := Random(G);
@@ -189,13 +222,13 @@ begin
    while (FinDePartie = Non) loop 
       Put(ASCII.ESC & "[2J"); -- Commande clear 
       case (I mod MaxJoueurs) is
-	 when 1 => J := Joueur1;
-       	 when 0 => J := Joueur2;
+	 when 1 => JoueurCourant := J1;
+       	 when 0 => JoueurCourant := J2;
 	 when Others => Put_Line("Erreur");
       end case;
-      Put_Line("C'est au tour du " & T_Joueur'Image(J));
+      Put_Line("C'est au tour du joueur " & To_String(JoueurCourant.Nom) & " (Symbole '" & JoueurCourant.Symbole & "')" );
       Affiche(Plateau);
-      Joue(Plateau,J);
+      Joue(Plateau,JoueurCourant);
 
       FinDePartie := Nul;
       FinDePartie := Gagne(Plateau);
@@ -206,7 +239,7 @@ begin
    Affiche(Plateau);
    
    case FinDePartie is 
-      when Victoire => Put_line("Victoire du " & T_Joueur'Image(J));
+      when Victoire => Put_line("Victoire du joueur " & JoueurCourant.Nom);
       when Egalite  => Put_Line("Match nul !!");
       when Non => Put_Line ("Wtf o_O ? Tu as planté mon programme !"); 
    end case;
